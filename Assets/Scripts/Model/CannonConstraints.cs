3661f18965ce7c937773bed2e86a0dbedb258271
Trindkr <mtot@itu.dk>
2025-10-25 20:14:31 +0200

    Add cannon values to config

---
+++ Assets/Scripts/Model/CannonConstraints.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Model
+{
+    [Serializable]
+    public struct CannonConstraints
+    {
+        public float ReloadTime;
+        public float ShootingForce;
+    }
+}
--- Assets/Scripts/ScriptableObjects/SimulationConfig.cs
+++ Assets/Scripts/ScriptableObjects/SimulationConfig.cs
@@ -11,5 +11,6 @@ namespace ScriptableObjects
         public int NumberOfPirateShips;
         public int NumberOfMerchants;
         public SailingConstraints  SailingConstraints;
+        public CannonConstraints   CannonConstraints;
     }
 }
