dee45ba55e22312ae27e7d91328b819f246e2649
oljh <oljh@itu.dk>
2025-10-30 16:21:42 +0100

    Obstacle avoidance raycasts

--- Assets/Scripts/Systems/ObstacleAvoidance.cs
+++ Assets/Scripts/Systems/ObstacleAvoidance.cs
@@ -20,31 +20,53 @@ namespace Systems
         {
             var collisionWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().CollisionWorld;
             float viewDistance = 20f;
+
+            var fleetMemberLookup = SystemAPI.GetComponentLookup<FleetMember>();
+            
             foreach (var (localTransform, navigation, fleetMember) 
                      in SystemAPI.Query<RefRO<LocalTransform>, RefRW<Navigation>, RefRO<FleetMember>>())
             {
+                // ReSharper disable once PossiblyImpureMethodCallOnReadonlyVariable
+                // Forward does not mutate the struct
                 float3 forward = localTransform.ValueRO.Forward();
+                float3 verticalOffset = new float3(0f, 2f, 0);
+                float3 raycastStart = localTransform.ValueRO.Position + verticalOffset;
+                float3 raycastEnd = raycastStart + forward * viewDistance;
                 var raycast = new RaycastInput
                 {
-                    Start = localTransform.ValueRO.Position,
-                    End = localTransform.ValueRO.Position + forward * viewDistance,
+                    Start = raycastStart,
+                    End = raycastEnd,
                     Filter = CollisionFilter.Default,
                 };
 
+                // Possibly make this a sphere cast for wider collision detection
                 if (collisionWorld.CastRay(raycast, out RaycastHit closestHit))
                 {
+                    // Has a fleet component
+                    if (fleetMemberLookup.TryGetComponent(closestHit.Entity, out var otherFleetMember))
+                    {
+                        if (otherFleetMember.FleetEntity == fleetMember.ValueRO.FleetEntity)
+                        {
+                           // Is same fleet - Do not use avoidance
+                            Debug.DrawLine(
+                                raycastStart, 
+                                closestHit.Position, 
+                                Color.red);
+                            continue;
+                        }
+                    }
+                    // Hit something that is not part of the fleet, should avoid
                     Debug.DrawLine(
-                        localTransform.ValueRO.Position, 
+                        raycastStart, 
                         closestHit.Position, 
                         Color.green);
+                    continue;
                 }
-                else
-                {
-                    Debug.DrawLine(
-                        localTransform.ValueRO.Position, 
-                        localTransform.ValueRO.Position + forward * viewDistance,
-                        Color.yellow);
-                }
+                // Hit nothing
+                Debug.DrawLine(
+                    raycastStart, 
+                    raycastEnd,
+                    Color.yellow);
             }
         }
     }
