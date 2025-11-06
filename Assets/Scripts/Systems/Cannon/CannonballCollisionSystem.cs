c61fb4d00d08c999051225daf5dad4e4c60984db
Maria Trindkær <mtot@itu.dk>
2025-11-06 13:35:08 +0100

    refactor and collision fix

--- Assets/Scripts/Systems/Cannon/CannonballCollisionSystem.cs
+++ Assets/Scripts/Systems/Cannon/CannonballCollisionSystem.cs
@@ -1,60 +1,63 @@
-using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using UnityEngine;
 
-[UpdateInGroup(typeof(PhysicsSystemGroup))]
-[UpdateAfter(typeof(PhysicsSimulationGroup))]
-public partial struct CannonballCollisionSystem : ISystem
+namespace Systems.Cannon
 {
-    public void OnCreate(ref SystemState state)
+    [UpdateInGroup(typeof(PhysicsSystemGroup))]
+    [UpdateAfter(typeof(PhysicsSimulationGroup))]
+    public partial struct CannonballCollisionSystem : ISystem
     {
-        state.RequireForUpdate<SimulationSingleton>();
-    }
-
-    public void OnUpdate(ref SystemState state)
-    {
-        var simulation = SystemAPI.GetSingleton<SimulationSingleton>().AsSimulation();
-        var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
-
-        simulation.FinalJobHandle.Complete();
-
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-
-        foreach (var collisionEvent in simulation.CollisionEvents)
+        public void OnCreate(ref SystemState state)
         {
-            Debug.Log("Collision detected");
-
-            Entity entityA = collisionEvent.EntityA;
-            Entity entityB = collisionEvent.EntityB;
-
-            bool aIsCannonball = SystemAPI.HasComponent<CannonballTag>(entityA);
-            bool bIsCannonball = SystemAPI.HasComponent<CannonballTag>(entityB);
-
-            bool aIsShip = SystemAPI.HasComponent<Ship>(entityA);
-            bool bIsShip = SystemAPI.HasComponent<Ship>(entityB);
-
-            if (aIsCannonball && bIsShip)
-            {
-                HandleHit(ref ecb, entityA, entityB);
-            }
-            else if (bIsCannonball && aIsShip)
-            {
-                HandleHit(ref ecb, entityB, entityA);
-            }
+            state.RequireForUpdate<PhysicsWorldSingleton>();
+            state.RequireForUpdate<SimulationSingleton>();
         }
 
-        ecb.Playback(state.EntityManager);
-        ecb.Dispose();
-    }
+        public void OnUpdate(ref SystemState state)
+        {
+            var simulation = SystemAPI.GetSingleton<SimulationSingleton>().AsSimulation();
+            var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
 
-    private void HandleHit(ref EntityCommandBuffer ecb, Entity cannonball, Entity ship)
-    {
-        Debug.Log($"Cannonball {cannonball} hit Ship {ship}");
+            simulation.FinalJobHandle.Complete();
 
-        ecb.AddComponent(ship, new Sinking());
-        //ecb.DestroyEntity(cannonball); //should cannonball be destroyed?
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var collisionEvent in simulation.CollisionEvents)
+            {
+                //Debug.Log("Collision detected");
+
+                Entity entityA = collisionEvent.EntityA;
+                Entity entityB = collisionEvent.EntityB;
+
+                bool aIsCannonball = SystemAPI.HasComponent<CannonballTag>(entityA);
+                bool bIsCannonball = SystemAPI.HasComponent<CannonballTag>(entityB);
+
+                bool aIsShip = SystemAPI.HasComponent<Ship>(entityA);
+                bool bIsShip = SystemAPI.HasComponent<Ship>(entityB);
+
+                if (aIsCannonball && bIsShip)
+                {
+                    HandleHit(ref ecb, entityA, entityB);
+                }
+                else if (bIsCannonball && aIsShip)
+                {
+                    HandleHit(ref ecb, entityB, entityA);
+                }
+            }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
+        }
+
+        private void HandleHit(ref EntityCommandBuffer ecb, Entity cannonball, Entity ship)
+        {
+            Debug.Log($"Cannonball {cannonball} hit Ship {ship}");
+
+            //ecb.AddComponent(ship, new Sinking());
+            //ecb.DestroyEntity(cannonball); //should cannonball be destroyed?
+        }
     }
 }
