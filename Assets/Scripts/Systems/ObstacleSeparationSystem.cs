274ed032c57d87d47fd6a756b807a6a0be51899c
oljh <oljh@itu.dk>
2025-12-04 14:24:44 +0100

    add IFE to Obstacle

--- Assets/Scripts/Systems/ObstacleSeparationSystem.cs
+++ Assets/Scripts/Systems/ObstacleSeparationSystem.cs
@@ -1,4 +1,5 @@
 using Components;
+using Components.Enum;
 using Systems.Fleet;
 using Unity.Burst;
 using Unity.Collections;
@@ -6,6 +7,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 namespace Systems
 {
@@ -18,6 +20,7 @@ namespace Systems
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
+            state.RequireForUpdate<JobModeSingleton>();
             _query =
                 SystemAPI.QueryBuilder()
                     .WithAll<Ship>()
@@ -30,13 +33,60 @@ namespace Systems
         {
             var transformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true);
             var ships = _query.ToEntityArray(Allocator.TempJob);
+         
             var job = new ObstacleSeparationJob
             {
                 Ships = ships,
                 ShipTransformLookup = transformLookup
             };
-            state.Dependency = job.ScheduleParallel(state.Dependency);
-            state.Dependency.Complete();
+            
+            var jobModeSingleton = SystemAPI.GetSingleton<JobModeSingleton>();
+            if (jobModeSingleton.JobMode == JobMode.Run)
+            {
+                foreach (var (localTransform, navigation, entity)
+                         in SystemAPI.Query<RefRO<LocalTransform>, RefRW<Navigation>>()
+                             .WithEntityAccess()
+                             .WithAll<Ship>())
+                {
+                    float3 separationForce = float3.zero;
+                    int neighborCount = 0;
+            
+                    foreach (Entity otherShipEntity in ships)
+                    {
+                        if (otherShipEntity == entity)
+                            continue;
+
+                        LocalTransform otherTransform = transformLookup[otherShipEntity];
+                        float3 toOther = otherTransform.Position - localTransform.ValueRO.Position;
+                        float distance = math.length(toOther);
+
+                        const float separationDistance = 10f;
+                        if (distance is < separationDistance and > 0f)
+                        {
+                            separationForce -= math.normalize(toOther) * (1f / distance);
+                            neighborCount++;
+                        }
+                    }
+
+                    if (neighborCount > 0)
+                    {
+                        separationForce /= neighborCount;
+                        var result = math.normalize(separationForce) * 100f;
+                        Debug.DrawLine(localTransform.ValueRO.Position, localTransform.ValueRO.Position + result, Color.magenta);
+                        navigation.ValueRW.DesiredDirection += result; 
+                    }
+                }
+            }
+            else if (jobModeSingleton.JobMode == JobMode.Schedule)
+            {
+                state.Dependency = job.Schedule(state.Dependency);
+                state.Dependency.Complete();
+            }
+            else
+            {
+                state.Dependency = job.ScheduleParallel(state.Dependency);
+                state.Dependency.Complete();
+            }
             ships.Dispose();
         }
 
@@ -48,13 +98,14 @@ namespace Systems
     }
     
     [BurstCompile]
+    [WithAll(typeof(Ship))]
     [WithNone(typeof(Sinking))]
     public partial struct ObstacleSeparationJob : IJobEntity
     {
         [ReadOnly] public NativeArray<Entity> Ships;
         [ReadOnly] public ComponentLookup<LocalTransform> ShipTransformLookup;
-        
-        public void Execute([ChunkIndexInQuery] int chunkIndex, in Entity entity, in LocalTransform transform, in Ship ship, ref Navigation navigation)
+
+        private void Execute(in Entity entity, in LocalTransform transform, ref Navigation navigation)
         {
             float3 separationForce = float3.zero;
             int neighborCount = 0;
@@ -75,7 +126,6 @@ namespace Systems
                     neighborCount++;
                 }
             }
-            
 
             if (neighborCount > 0)
             {
